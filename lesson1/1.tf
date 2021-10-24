1ba2f09aa5ca674147d23129ca3689dd857935b5
Jo <66825013+Usaldudo@users.noreply.github.com>
2021-10-24 17:16:05 +0600

    lesson 2

--- lesson1/1.tf
+++ lesson1/1.tf
@@ -2,8 +2,14 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_instance" "ubuntu-1"  {
-    count = 2
-    ami = "ami-0ff338189efb7ed37"
-    instance_type = "t3.micro"
+resource "aws_instance" "ubuntu-1" {
+  count = 1
+  ami   = "ami-0ff338189efb7ed37" # ubuntu
+  # ami = "ami-0d15082500b576303" # amazon
+  instance_type = "t3.micro"
+  key_name      = "pc"
+  tags = {
+    Name  = "web-servers"
+    Owner = "me"
+  }
 }
